2accbbbf44a7f726ebdfa4c81bc51e0b0d068af8
adaorajoelah <adaora.onwubuariri@binghamuni.edu.ng>
2025-07-25 03:10:17 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -2,22 +2,27 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+
+# Configure to use MySQL instead of SQLite
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/flask_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
-# Model
+# Task model
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     description = db.Column(db.String(200))
     status = db.Column(db.String(20), default='Pending')
 
-# Routes
+# Home route
 @app.route('/')
 def index():
     tasks = Task.query.all()
     return render_template('index.html', tasks=tasks)
 
+# Create route
 @app.route('/create', methods=['GET', 'POST'])
 def create():
     if request.method == 'POST':
@@ -29,6 +34,7 @@ def create():
         return redirect('/')
     return render_template('create.html')
 
+# Update route
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     task = Task.query.get_or_404(id)
@@ -40,6 +46,7 @@ def update(id):
         return redirect('/')
     return render_template('update.html', task=task)
 
+# Delete route
 @app.route('/delete/<int:id>')
 def delete(id):
     task = Task.query.get_or_404(id)
@@ -47,7 +54,9 @@ def delete(id):
     db.session.commit()
     return redirect('/')
 
+# Run the app
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()
+        db.create_all()  # Creates the tables in MySQL
     app.run(debug=True)
+
